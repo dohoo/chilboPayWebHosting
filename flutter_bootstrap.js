635b67e27b3e350d3e1c7c834438c74ae5e84f70
dohoo <dohoo2007@naver.com>
2024-12-18 00:19:21 +0900

    add --releases

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1504932595"
+    serviceWorkerVersion: "3234044188"
   }
 });
